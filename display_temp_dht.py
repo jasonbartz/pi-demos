ad1b136b4d580f5edf629ce97f3e17ed2bf70b12
Jason Bartz <jrbartz@gmail.com>
2016-07-23 11:36:43 -0400

    actually displaying stuff

--- display_temp_dht.py
+++ display_temp_dht.py
@@ -13,11 +13,13 @@ PIN = 4
 display = AlphaNum4.AlphaNum4()
 display.begin()
 
+
 def c_to_f(c):
         return c * 9.0 / 5.0 + 32.0
 
 display.clear()
 display.write_display()
+
 
 def write_display(text):
     display.print_str(text)
@@ -26,8 +28,10 @@ def write_display(text):
 
 print 'Press Ctrl-C to quit.'
 while True:
-    # display.clear()
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    display.clear()
+    humidity, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(c_to_f(temperature), humidity))
-
+    write_display("{:0.0f}*".format(temperature))
+    time.sleep(5)
+    write_display("{:0.0f}%".format(humidity))
     time.sleep(5)
